876e8fe514feb344f4a0498ef932fd21b798c74e
Jenifer Maria <msn.da.jenny@gmail.com>
2018-05-16 15:48:59 -0300

    Comentários no Ola Mundo Java

---
+++ cursoJavaOO/fundamentosLinguagem/OlaMundo.java
@@ -0,0 +1,17 @@
+public class OlaMundo {
+
+    public static void main(String[] args) {
+        // A linha abaixo imprime o texto dentro dos () na tela
+        /* 
+        Esta linha será ignorada pelo compilador  
+        */
+        System.out.println("Olá Mundo");
+
+        // sequencias de escape
+        System.out.println("Olá \"Mundo\"");
+        System.out.println("Olá \\Mundo\\");
+
+        // \n pula linha 
+        System.out.println("Olá \nMundo");
+    }
+}
