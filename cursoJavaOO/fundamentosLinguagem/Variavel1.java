dfbc12cab8ffdeaf64435bcf68365e820c4606ff
Jenifer Maria <msn.da.jenny@gmail.com>
2018-05-16 15:56:54 -0300

    Criando Variaveis

---
+++ cursoJavaOO/fundamentosLinguagem/Variavel1.java
@@ -0,0 +1,13 @@
+public class Variavel1 {
+    public static void main(String[] args) {
+        int quantidade; // declarando variavel inteiro
+        quantidade = 10; // atrubuindo o valor 10
+
+        System.out.println(quantidade);
+
+        quantidade = 15;
+
+        System.out.println(quantidade);
+        
+    }
+}
